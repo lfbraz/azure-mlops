e8a07422629f132b03052bb4e618de0a118d2567
Luiz Braz <luiz.braz@live.com>
2020-07-22 18:38:18 -0300

    Update model

--- notebooks/train.py
+++ notebooks/train.py
@@ -123,5 +123,5 @@ NOME_MODELO_DEPLOY = 'model-regressao-tensorflow.h5'
 NOME_ARQ_STATS = 'train_stats.pkl'
 
 save_model(model, stats, NOME_MODELO_DEPLOY, DIRETORIO_MODELO, NOME_ARQ_STATS)
-print('done')
+
 print('done')
